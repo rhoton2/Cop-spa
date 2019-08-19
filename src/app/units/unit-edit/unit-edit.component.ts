885911dd241c67ffb96083bae123abd5a639e552
rrhoton@student.king.edu <11!!RR0ger2Army>
2019-08-19 01:40:55 -0500

    Reads and inputs data for Basic unit data

--- src/app/units/unit-edit/unit-edit.component.ts
+++ src/app/units/unit-edit/unit-edit.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { UnitService } from '../../_services/unit.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { Unit } from 'src/app/_models/unit';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 
@@ -13,28 +14,52 @@ export class AppModule { }
   templateUrl: './unit-edit.component.html',
   styleUrls: ['./unit-edit.component.css']
 })
+
+
+
+
 export class UnitEditComponent implements OnInit {
-  @Input() id: Unit;
-  @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+
   query: string;
+  model: any;
+  @Input() id: number;
+  @Output() cancelRegister = new EventEmitter();
+
+
   unitData: Array<Unit>;
 
-  constructor(private unitService: UnitService, private alertify: AlertifyService) { }
+  constructor(private unitService: UnitService, private alertify: AlertifyService,private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
-
+    this.activatedRoute.params.subscribe(paramsId => {
+      this.id = paramsId.id;
+  });
+    if (isNaN(this.id) || this.id <= 0) {
+      this.router.navigate(['unit/edit']);
+      return;
   }
+
+      this.getUnit(this.id);
+  }
+
+  search(): void {
+    this.unitService.getAll().subscribe(
+      (data: any) => { this.model = data; },
+      error => console.log(error)
+    );
+  }
+
+ 
   getUnit(id: number): void {
     this.unitService.getUnit(id).subscribe(
-      (data: any) => { this.unitData = data; },
+      (data: any) => { this.model = data; },
       error => console.log(error)
     );
   }
 
   addNewUnit() {
     this.unitService.addUnit(this.model).subscribe(() => {
-      this.alertify.success('registration successful');
+      this.alertify.success('Unit Saved');
     }, error => {
       this.alertify.error(error);
     });
